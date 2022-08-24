e321c1002a04f44983d83796d4b517679b575163
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-24 20:27:25 -0300

    Adicionando 02 - React Avançado - API

---
+++ 02 - React Avançado - API/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+import {ThemeContext, themes} from './Theme'
+import Card from './Card'
+
+
+function App ()
+{
+  const [token, setToken] = useState()
+
+  useEffect(() => 
+  {
+    setTimeout(() =>
+    {
+      setToken('dfdfshkasdlksjdj76jsdaskd')
+    }, 4000)
+  }, [setToken])
+
+  return (
+      <ThemeContext.Provider value={{...themes.primary, token}}>
+       <Card />
+      </ThemeContext.Provider>
+    )
+}
+
+export default App
