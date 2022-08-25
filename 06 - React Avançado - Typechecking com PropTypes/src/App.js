22a3b6ee19314ed64a27cb1bedba49c66ae3b0a3
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-24 21:19:08 -0300

    Adicionando 06 - React Avançado - Typechecking com PropTypes

---
+++ 06 - React Avançado - Typechecking com PropTypes/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PetShop from './PetShop'
+
+
+function App () 
+{
+  const handleClick = () =>
+  {
+    console.log('Iniciando o banho...')
+  }
+
+  return(
+    <PetShop
+      dogs={2}
+      customerName="Luciano da Cruz Junior"
+      onClick={handleClick}
+      status="done"
+    />
+  )
+}
+
+export default App
