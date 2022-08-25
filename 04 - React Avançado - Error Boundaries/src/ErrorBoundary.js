26483230c33ecd8e95597ebab5ff145d8e82a21d
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-24 21:10:15 -0300

    Adicionando 04 - React Avançado - Error Boundaries

---
+++ 04 - React Avançado - Error Boundaries/src/ErrorBoundary.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ErrorImg from './logo.svg'
+
+class ErrorBoundary extends Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.state = 
+        {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, errorInfo)
+    {
+        console.log('error', error)
+        console.log('errorInfo', errorInfo)
+        this.setState({
+            hasError: true
+        })
+    }
+
+    render ()
+    {
+        if(this.state.hasError)
+        {
+            return <img src={ErrorImg} />
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary
---
+++ 04 - React Avançado - Error Boundaries/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from './ErrorBoundary';
+
+const main = 
+(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {main}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportW ebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
